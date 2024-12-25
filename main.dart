d9ff2b9e0f966d64b48ca6dd0d37440aa1580513
Muhammad Bilal khan <155465703+m-bilal-1@users.noreply.github.com>
2024-12-25 20:24:14 +0600

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,14 @@
+//main.dart
+import 'package:flutter/material.dart';
+import 'package:login_page/flash_screen.dart';
+
+import 'login.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: flash_screen(),
+  ));
+}
+
